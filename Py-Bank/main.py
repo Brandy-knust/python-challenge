b83ceda1c1ac3615ddc154a096acc43748222f97
Brandy Knust <brandy_1175@hotmail.com>
2021-01-31 14:32:09 -0600

    commit 1-31-2

--- Py-Bank/main.py
+++ Py-Bank/main.py
@@ -4,7 +4,7 @@ import os
 #import the module for reading the CSV file
 import csv
 
-csvpath = os.join.path("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("..", "Resources", "budget_data.csv")
 
 #Method 2: Improved Reading of CSV Module
 
@@ -12,6 +12,10 @@ with open(csvpath) as csvfile:
 
     #CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
     print(csvreader)
+    #Read the header row first
+    csv_header = next(csvreader)
+
+    print(csv_header{1})
+    print(len(csvfile))
     
