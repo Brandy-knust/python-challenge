2b4d63a7ad6ab36997581643fae4978e451bcd27
Brandy Knust <brandy_1175@hotmail.com>
2021-01-31 12:07:51 -0600

    commit 1-31

---
+++ Py-Bank/main.py
@@ -0,0 +1,17 @@
+#import the os module
+import os
+
+#import the module for reading the CSV file
+import csv
+
+csvpath = os.join.path("..", "Resources", "budget_data.csv")
+
+#Method 2: Improved Reading of CSV Module
+
+with open(csvpath) as csvfile:
+
+    #CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+    
