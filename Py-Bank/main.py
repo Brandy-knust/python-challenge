1731a2f1ad604cca4b70113c43065b40f4248015
Brandy Knust <brandy_1175@hotmail.com>
2021-02-04 17:19:55 -0600

    2-4 commit

--- Py-Bank/main.py
+++ Py-Bank/main.py
@@ -6,50 +6,44 @@ import csv
 
 csvpath = os.path.join('..', 'Py-Bank','Resources', 'budget_data.csv')
 
-#lists to store data
-month = []
-Profit_Loss = []
-
-#set counter
-month_counter = 0
-Profit_Loss = 0
-difference_profit = 0
-Average_prof = 0
 
 #Method 2: Improved Reading of CSV Module
+
 
 with open(csvpath, newline='') as csvfile:
 
     #CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-    #Read the header row first
+    print(csvreader)
+
     csv_header = next(csvreader)
+   
+    #lists to store data
+    month = []
+    Profit = []
+    Profit.append(0)
+    Diff = []
+   
     for row in csvreader:
-     #to check that it's working
-        #print (row)
 
-        #Add month
-        month.append(row[0])
+        #make lists into values
+        month.append(str(row[0]))
 
-        #Add Profit/Losses
-        Profit_Loss.append(row[1])
-        
-        #count months
-        month_counter = month_counter + 1
-        #print (month_counter) to make sure it works
+        Profit.append(int(row[1]))
 
-        Profit_Loss = Profit_Loss + 1
-        print (Profit_Loss)
+        #Michael Thomas helped me think of the Diff.append
+        Diff.append(Profit[-1] - Profit[-2])
 
-#   Financial Analysis
-#   ----------------------------
-#   Total Months: 
-#   Total: 
-#   Average  Change: 
-#   Greatest Increase in Profits: 
-#   Greatest Decrease in Profits: 
-
+    Profit.pop(0)
+    #print financial analysis
+    print(f'Financial Analysis')
+    print(f'----------------------------------')
+    print(f'Total Months: {len(month)}')
+    print(f'Total: {sum(Profit)}')
+    print(f'Average Change: {sum(Diff)/len(month)}')
+    print(f'Greatest Increase in Profits: {month[Diff.index(max(Diff))]} {max(Diff)}')
+    print(f'Greatest Decrease in Profits: {month[Diff.index(min(Diff))]} {min(Diff)}')
+    
 
 
         
