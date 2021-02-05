bcdcb6bc1773f43b05d979b16c7824f2dd12c1c8
Brandy Knust <brandy_1175@hotmail.com>
2021-02-04 21:47:47 -0600

    commit 2-4

--- Py-Poll/main.py
+++ Py-Poll/main.py
@@ -16,3 +16,28 @@ with open(csvpath, newline='') as csvfile:
     print(csvreader)
 
     csv_header = next(csvreader)
+
+Total_Votes = 0
+Candidate_Votes = 1
+Name_list = []
+
+Voter_ID = []
+County = []
+Candidate = []
+
+
+
+
+
+
+#print analysis block
+print (f'Election Results')
+print (f'-----------------------------')
+#pring total votes
+print (f'Total Votes {sum(Total_Votes)}')
+print (f'-----------------------------')
+#print percentages votes for each candidate
+print (f'')
+print (f'-----------------------------')
+#print winner
+print (f'')
