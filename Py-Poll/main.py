c10ea19a202f147326213788f1383cf2bec3618a
Brandy Knust <brandy_1175@hotmail.com>
2021-02-06 10:03:33 -0600

    commit 2-6

--- Py-Poll/main.py
+++ Py-Poll/main.py
@@ -17,36 +17,60 @@ with open(csvpath, newline='') as csvfile:
 
     csv_header = next(csvreader)
 
-#lists to sort and store
+    #lists to sort and store
 
-Total_Votes = 0
-Candidate_Votes = 1
-Name_list = []
+    Total_Votes = 0
+    Candidate_Votes = 1
+    Name_list = []
+    Khan = []
+    Li = []
+    Correy = []
+    OTooley = []
 
-#dictionary
-candidate_names = {}
-candidate_names = dict()
+    #dictionary
+    candidate_names = {}
+    candidate_names = {"Khan": 0, "Li": 0, "Correy": 0, "O'Tooley": 0}
 
-Voter_ID = []
-County = []
-Candidate = []
+    Voter_ID = []
+    County = []
+    Candidate = []
 
-for row in csvreader:
+    for row in csvreader:
 
     #makes lists into values
-    Voter_ID.append(str(row[0]))
-    County.append(str(row[1]))
-    Candidate.append(str(row[2]))
+        Voter_ID.append(str(row[0]))
+        County.append(str(row[1]))
+        Candidate.append(str(row[2]))
 
     #find votes by candidate
-    
+        for x in candidate_names:
+            if isinstance(candidate_names[x], list):
+                count += len(candidate_names[x])
+    print(count)        
+
+
+        # Khan.append(int(row[2])) 
+        # Li.append(int(row[2]))
+        # Correy.append(int(row[2]))
+        # OTooley.append(int(row[2]))
+        # if [2] is "Khan":
+        #     sum(Khan)
+            
+        # elif [2] is "Li":
+        #     sum(Li)
+        # elif [2] is "Correy":
+        #     sum(Correy)
+        
+        # else:
+        #     sum(OTooley)
+               
 
 
 
 # #print analysis block
 # print (f'Election Results')
 # print (f'-----------------------------')
-# #pring total votes
+# #print total votes
 # print (f'Total Votes {sum(Total_Votes)}')
 # print (f'-----------------------------')
 # #print percentages votes for each candidate
@@ -55,5 +79,5 @@ for row in csvreader:
 # #print winner
 # print (f'')
 
-print (f'{candidate_names}')
+
 
