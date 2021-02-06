ab1e2868ab7da096c22170cb8f3db897573abf5b
Brandy Knust <brandy_1175@hotmail.com>
2021-02-06 14:58:23 -0600

    commit 2-6

--- Py-Bank/main.py
+++ Py-Bank/main.py
@@ -5,7 +5,7 @@ import os
 import csv
 
 csvpath = os.path.join('..', 'Py-Bank','Resources', 'budget_data.csv')
-
+csvoutpath = os.path.join('..', 'Py-Bank', 'Analysis','Financial_Analysis.txt')
 
 #Method 2: Improved Reading of CSV Module
 
@@ -32,22 +32,46 @@ with open(csvpath, newline='') as csvfile:
 
         #Michael Thomas helped me think of the Diff.append
        
-        if not First:   
+        if not First == True:  
             Diff.append(Profit[-1] - Profit[-2])
         else: 
             First = False
 
 
+    results = (
+    f'Financial Analysis \n'
+    f'---------------------------------- \n'
+    f'Total Months: {len(month)} \n'
+    f'Total: {sum(Profit)} \n'
+    f'Average Change: {sum(Diff)/len(month):.2f} \n'
+    f'Greatest Increase in Profits: {month[Diff.index(max(Diff))]} {max(Diff)} \n'
+    f'Greatest Decrease in Profits: {month[Diff.index(min(Diff))]} {min(Diff)} \n'
+    )
+
+
+    print(results)
+
+    with open (csvoutpath, "w") as text:
+        text.write(results)
+
     #print financial analysis
-    print(f'Financial Analysis')
-    print(f'----------------------------------')
-    print(f'Total Months: {len(month)}')
-    print(f'Total: {sum(Profit)}')
-    print(f'Average Change: {sum(Diff)/len(month):.2f}')
-    print(f'Greatest Increase in Profits: {month[Diff.index(max(Diff))]} {max(Diff)}')
-    print(f'Greatest Decrease in Profits: {month[Diff.index(min(Diff))]} {min(Diff)}')
+    
+    # print(f'Financial Analysis')
+    # print(f'----------------------------------')
+    # print(f'Total Months: {len(month)}')
+    # print(f'Total: {sum(Profit)}')
+    # print(f'Average Change: {sum(Diff)/len(month):.2f}')
+    # print(f'Greatest Increase in Profits: {month[Diff.index(max(Diff))]} {max(Diff)}')
+    # print(f'Greatest Decrease in Profits: {month[Diff.index(min(Diff))]} {min(Diff)}')
     
 
-with open(py-bank)
-        
+# with open (csvoutpath, "w") as text:
+#     text.write(f'Financial Analysis\n')
+#     text.write(f'----------------------------------\n')
+#     text.write(f'Total Months: {len(month)}\n')
+#     text.write(f'Total: {sum(Profit)}\n')
+#     text.write(f'Average Change: {sum(Diff)/len(month):.2f}\n')
+#     text.write(f'Greatest Increase in Profits: {month[Diff.index(max(Diff))]} {max(Diff)}\n')
+#     text.write(f'Greatest Decrease in Profits: {month[Diff.index(min(Diff))]} {min(Diff)}\n')
+
     
