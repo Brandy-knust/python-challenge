978c2968127184d923c1ca59c03497b4c29b1115
Brandy Knust <brandy_1175@hotmail.com>
2021-02-05 18:16:31 -0600

    commit 2-5

--- Py-Poll/main.py
+++ Py-Poll/main.py
@@ -17,27 +17,43 @@ with open(csvpath, newline='') as csvfile:
 
     csv_header = next(csvreader)
 
+#lists to sort and store
+
 Total_Votes = 0
 Candidate_Votes = 1
 Name_list = []
+
+#dictionary
+candidate_names = {}
+candidate_names = dict()
 
 Voter_ID = []
 County = []
 Candidate = []
 
+for row in csvreader:
+
+    #makes lists into values
+    Voter_ID.append(str(row[0]))
+    County.append(str(row[1]))
+    Candidate.append(str(row[2]))
+
+    #find votes by candidate
+    
 
 
 
+# #print analysis block
+# print (f'Election Results')
+# print (f'-----------------------------')
+# #pring total votes
+# print (f'Total Votes {sum(Total_Votes)}')
+# print (f'-----------------------------')
+# #print percentages votes for each candidate
+# print (f'')
+# print (f'-----------------------------')
+# #print winner
+# print (f'')
 
+print (f'{candidate_names}')
 
-#print analysis block
-print (f'Election Results')
-print (f'-----------------------------')
-#pring total votes
-print (f'Total Votes {sum(Total_Votes)}')
-print (f'-----------------------------')
-#print percentages votes for each candidate
-print (f'')
-print (f'-----------------------------')
-#print winner
-print (f'')
